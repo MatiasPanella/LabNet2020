d1a1c280371416adbcff2315f7fd5caa9e3edca7
MatiasPanella <matiaspanella98@gmail.com>
2020-11-02 15:14:47 -0300

    first excersice with entity freamwork

---
+++ Lab.Capas/Lab.Capas.Presentacion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Lab.Demo.Logic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Capas.Presentacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegionLogic regionLogic = new RegionLogic();
+            var regiones = regionLogic.Regions();
+        }
+    }
+}
---
+++ Lab.Capas/Lab.Demo.Logic/RegionLogic.cs
@@ -0,0 +1,19 @@
+﻿using Lab.Demo.Entities;
+using Lab.Demo.ResourceAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Demo.Logic
+{
+    public class RegionLogic
+    {
+        public List<Region> Regions()
+        {
+            NorthwindContext context = new NorthwindContext();
+            return context.Region.ToList();
+        }
+    }
+}
