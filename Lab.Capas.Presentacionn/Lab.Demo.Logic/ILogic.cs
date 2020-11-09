8de471fde3daa977fe4a109af978aff32dac2d0b
MatiasPanella <matiaspanella98@gmail.com>
2020-11-09 15:42:40 -0300

    commit about practice 3 abm with ef

---
+++ Lab.Capas.Presentacionn/Lab.Demo.Logic/ILogic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Demo.Logic
+{
+    interface ILogic<T>
+    {
+        List<T> GetAll();
+
+        T GetOne(int id);
+
+        T Inster(T entity);
+
+        T Update(T entity);
+
+        T Delete(int id);
+
+    }
+}
