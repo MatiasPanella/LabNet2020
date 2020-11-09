78a75431e968c281687e28aa1d8dd008267c18f9
MatiasPanella <matiaspanella98@gmail.com>
2020-11-09 19:08:12 -0300

    commit excersice 4 HTML, CSS, JS

---
+++ Lab.Form/Lab.Form/Scripts/index.js
@@ -0,0 +1,18 @@
+﻿$("#btn-submit").on('click', () => {
+    console.log("hola");
+    var name = $("#name").val();
+    var surname = $("#surename").val();
+    if (name == null || name.length== 0) {
+        if (surname == null || surname.length == 0) {
+            alert("Falta cargar nombre y apellido")
+        } else {
+            alert("Falta cargar el nombre")
+        }
+    } else  if (surname == null || surname.length == 0) {
+            alert("falta cargar apellido")
+        } else {
+            alert("Cargado")
+        }
+    
+})
+
