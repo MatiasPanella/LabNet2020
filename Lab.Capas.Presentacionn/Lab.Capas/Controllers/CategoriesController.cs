dd735cfe51842ce9642573ba02bc4800b4c332de
MatiasPanella <matiaspanella98@gmail.com>
2020-11-11 17:38:54 -0300

    abm with mvc

---
+++ Lab.Capas.Presentacionn/Lab.Capas/Controllers/CategoriesController.cs
@@ -0,0 +1,59 @@
+﻿using Lab.Demo.Entities;
+using Lab.Demo.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab.Capas.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            var logic = new CategoriesLogic();
+            var categories = logic.GetAll();
+            return View(categories);
+        }
+        [HttpGet]
+        public ActionResult Insert()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Insert(Categories category)
+        {
+            var logic = new CategoriesLogic();
+            var categoryEntity = new Categories() { CategoryName = category.CategoryName, Description = category.Description };
+            logic.Insert(categoryEntity);
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit (int id)
+        {
+            CategoriesLogic logic = new CategoriesLogic();
+            Categories categories = logic.GetOne(id);
+            return View(categories);
+        }
+
+        [HttpPost]
+        public ActionResult Edit (Categories categories)
+        {
+            var logic = new CategoriesLogic();
+            logic.Update(categories);
+            return RedirectToAction("index");
+        }
+        public ActionResult Delete(int id)
+        {
+            var logic = new CategoriesLogic();
+            logic.Delete(id);
+            return RedirectToAction("index");
+        }
+
+
+    }
+}
---
+++ Lab.Capas.Presentacionn/Lab.Capas/Controllers/EmployeesController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Lab.Demo.Entities;
+using Lab.Demo.Logic;
+
+namespace Lab.Capas.Controllers
+{
+    public class EmployeesController : Controller
+    {
+
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            var logic = new EmployeesLogic();
+            var employees = logic.GetAll();
+            return View(employees);
+        }
+        
+        [HttpGet]
+        public ActionResult Insert()
+        {
+            return View();
+        }
+            
+        [HttpPost]
+        public ActionResult Insert(Employees employee)
+        {
+            var logic = new EmployeesLogic();
+            var employeeEntity = new Employees() { FirstName = employee.FirstName, LastName= employee.LastName};
+            logic.Insert(employeeEntity);
+            return RedirectToAction("index");
+            
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var logic = new EmployeesLogic();
+            logic.Delete(id);
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            EmployeesLogic logic = new EmployeesLogic ();
+            Employees employees = logic.GetOne(id);
+            return View(employees);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employees employees)
+        {
+            var logic = new EmployeesLogic();
+            logic.Update(employees);
+            return RedirectToAction("index");
+        }
+
+    }
+}
